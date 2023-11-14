d9aa40f401d6a4228bf0f371c5c6d1c56c38e0c6
thiago-rocco <deroccotr@gmail.com>
2023-11-14 07:20:14 -0300

    Inserido dados na tabela entradas

--- 03-controle-de-estoque-simples/03-Inserindo-registros.sql
+++ 03-controle-de-estoque-simples/03-Inserindo-registros.sql
@@ -3,5 +3,12 @@ INSERT INTO produtos
 VALUES
     ('Relógio',24.99),
     ('Grampeador',11.49),
-    ('Mouse',8.99)
+    ('Mouse',8.99);
+
+INSERT INTO entradas
+    (produto_id, quantidade, data_entrada)
+VALUES
+    (1,200,'2023-11-03'),
+    (2,500,'2023-11-04'),
+    (3,150,'2023-11-10');
 
