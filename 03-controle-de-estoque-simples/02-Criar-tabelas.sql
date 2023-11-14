a00c8b604513aa797f61d32536421b611e702ac4
thiago-rocco <deroccotr@gmail.com>
2023-11-14 06:58:27 -0300

    Inserida as instruções para a criação das tabelas produtos, entradas e saidas do banco de dados estoque

---
+++ 03-controle-de-estoque-simples/02-Criar-tabelas.sql
@@ -0,0 +1,21 @@
+CREATE TABLE produtos (
+    produto_id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255),
+    valor_unitario DECIMAL(10,2)
+);
+
+CREATE TABLE entradas(
+    entrada_id INT PRIMARY KEY AUTO_INCREMENT,
+    produto_id INT,
+    quantidade INT,
+    data_entrada DATE,
+    FOREIGN KEY (produto_id) REFERENCES produtos(produto_id)
+)
+
+CREATE TABLE saidas(
+    saida_id INT PRIMARY KEY AUTO_INCREMENT,
+    produto_id INT,
+    quantidade INT,
+    data_saida DATE,
+    FOREIGN KEY (produto_id) REFERENCES produtos(produto_id)
+)
